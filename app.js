323c43a9c8dee59e8c4653b643cb219dfcc67568
John Smith <assaf@youappi.com>
2012-12-24 18:55:10 +0200

    WebRTC + Bootstrap boiler plate. To run server do: node app.js

---
+++ app.js
@@ -0,0 +1,80 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 8000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+var webRTC = require('webrtc.io').listen(app);
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+webRTC.rtc.on('connect', function(rtc) {
+  console.log('Client connected');
+});
+
+webRTC.rtc.on('send answer', function(rtc) {
+  console.log('answer sent');
+});
+
+webRTC.rtc.on('disconnect', function(rtc) {
+  //Client disconnect 
+});
+
+webRTC.rtc.on('chat_msg', function(data, socket) {
+  var roomList = webRTC.rtc.rooms[data.room] || [];
+
+  for (var i = 0; i < roomList.length; i++) {
+    var socketId = roomList[i];
+
+    if (socketId !== socket.id) {
+      var soc = webRTC.rtc.getSocket(socketId);
+
+      if (soc) {
+        soc.send(JSON.stringify({
+          "eventName": "receive_chat_msg",
+          "data": {
+            "messages": data.messages,
+            "color": data.color
+          }
+        }), function(error) {
+          if (error) {
+            console.log(error);
+          }
+        });
+      }
+    }
+  }
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
