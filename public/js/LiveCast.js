6c3c8a5a07a97d849a52e23e21a6c0bddfc12b80
orizens <farhioren@gmail.com>
2012-12-25 15:26:52 +0200

    added web rtc protocol for client

---
+++ public/js/LiveCast.js
@@ -0,0 +1,28 @@
+var LiveCast = {
+  Views: {}
+};
+
+
+//- send data to server
+LiveCast.send = function(eventName, data, callback) {
+  rtc._socket.send(JSON.stringify({
+    "eventName": eventName,
+    "data": data
+  }), callback);
+};
+
+// events to communicate with server nodejs
+LiveCast.SendEvents = {
+  CHAT: 'chat_msg',
+  CODE: 'code_editor',
+  QUESTION: 'raise_hand',
+  LOGIN: 'login',
+  DISCONNECT: 'logout'
+};
+
+LiveCast.RecieveEvents = {
+  QUESTION: 'hand_raised',
+  CHAT: 'chat_msg',
+  CONNECTED: 'on_login',
+  DISCONNECTED: 'on_logout'
+};
