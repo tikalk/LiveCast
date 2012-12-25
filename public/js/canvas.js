fff75d1707f7937d83d36cf4eb470a09c2bfffa8
Mark Vasilkov <mvasilkov@gmail.com>
2012-12-25 13:59:35 +0200

    Tabs to spaces

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -55,36 +55,36 @@
     }
 
     function initialize() {
-    	console.log("Initialize")
+        console.log("Initialize")
             
-    	rtc.connect("ws://" + location.host, ROOM)
+        rtc.connect("ws://" + location.host, ROOM)
             
-    	rtc.on("add remote stream", function(stream, socket) {
-    	    console.log("Add remote stream")
-    	})
+        rtc.on("add remote stream", function(stream, socket) {
+            console.log("Add remote stream")
+        })
             
-    	rtc.on("disconnect stream", function(stream, socket) {
-    	    console.log("Disconnect stream")
-    	})
+        rtc.on("disconnect stream", function(stream, socket) {
+            console.log("Disconnect stream")
+        })
             
-    	rtc.on("receive_canvas_line", function(obj) {
-    	    console.log("Receive canvas line", obj)
-    	    drawLine(obj.start_point[0], obj.start_point[1], obj.end_point[0], obj.end_point[1], obj.color)
-    	})
+        rtc.on("receive_canvas_line", function(obj) {
+            console.log("Receive canvas line", obj)
+            drawLine(obj.start_point[0], obj.start_point[1], obj.end_point[0], obj.end_point[1], obj.color)
+        })
             
-    	$("test_button").addEventListener("click", function(event) {
-    	    console.log("Test button click")
+        $("test_button").addEventListener("click", function(event) {
+            console.log("Test button click")
                 
-    	    rtc._socket.send(JSON.stringify({
-        		eventName: "canvas_line",
-        		data: {
-        		    color: "#ff0080",
-        		    start_point: [0, 0],
-        		    end_point: [250, 0],
-        		    room: ROOM
-        		}
-    	    }), function(err) { if (err) console.log(err) })
-    	}, false)
+            rtc._socket.send(JSON.stringify({
+                eventName: "canvas_line",
+                data: {
+                    color: "#ff0080",
+                    start_point: [0, 0],
+                    end_point: [250, 0],
+                    room: ROOM
+                }
+            }), function(err) { if (err) console.log(err) })
+        }, false)
     }
 
     this.canvasMod = {
