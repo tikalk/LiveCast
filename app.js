11dcf028504bffb3a163bf5dd0658b2f1d483144
Naor <naor.biton@gmail.com>
2012-12-25 13:36:05 +0200

    Added raise_hand and hand_raised handling

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ app.listen(8000);
 console.log('Server listening to port: '+8000);
 var webRTC = require('webrtc.io').listen(app);
 
+var usersNum = 0;
 var nicknames = {};
+var roomAdmin;
 
 app.configure(function(){
   app.set('port', process.env.PORT || 8000);
@@ -25,6 +27,31 @@ app.get('/', function(req, res) {
 app.get('/users', function(req, res){
     res.send(JSON.stringify(nicknames));
 });
+
+// Socket messaging helper functions
+function emitPeerEvent(socketId, eventName, data){
+    var soc = webRTC.rtc.getSocket(socketId);
+
+    if (soc) {
+        soc.send(JSON.stringify({
+            "eventName": eventName,
+            "data": data
+        }), function(error) {
+            if (error) {
+                console.log(error);
+            }
+        });
+    }
+}
+
+function doForCurrentRoom(fn){
+    var roomList = webRTC.rtc.rooms[data.room] || [];
+
+    for (var i = 0; i < roomList.length; i++) {
+        var socketId = roomList[i];
+        fn(socketId);
+    }
+}
 
 webRTC.rtc.on('connect', function(rtc) {
   console.log('Client connected');
@@ -49,6 +76,7 @@ webRTC.rtc.on('disconnect', function(rtc) {
     if (! isFound){
       console.log("Disconnected Nickname: "+nicknames[socketId]);
       delete nicknames[nicknames[socketId]];
+      usersNum--;
       break;
     }
     
@@ -57,33 +85,45 @@ webRTC.rtc.on('disconnect', function(rtc) {
 
 
 webRTC.rtc.on('login', function(data, socket){
-  console.log('Login: '+socket.id);
-  console.log("Nickname: "+data.nickname);
-  nicknames[socket.id] = data.nickname;
+    if (usersNum == 0){
+
+        // Setting admin role
+        roomAdmin = {
+            'socketId' : socket.id,
+            'nickname' : data.nickname
+        }
+
+        socket.on('give_ros', function(data, socket){
+
+        });
+    } else {
+        console.log('Login: '+socket.id);
+        console.log("Nickname: "+data.nickname);
+        nicknames[socket.id] = data.nickname;
+    }
+
+    usersNum++;
 });
+
+
 
 webRTC.rtc.on('chat_msg', function(data, socket) {
-  var roomList = webRTC.rtc.rooms[data.room] || [];
-
-  for (var i = 0; i < roomList.length; i++) {
-    var socketId = roomList[i];
-
-    if (socketId !== socket.id) {
-      var soc = webRTC.rtc.getSocket(socketId);
-
-      if (soc) {
-        soc.send(JSON.stringify({
-          "eventName": "receive_chat_msg",
-          "data": {
-            "messages": data.messages,
-            "color": data.color
-          }
-        }), function(error) {
-          if (error) {
-            console.log(error);
-          }
-        });
-      }
-    }
-  }
+    doForCurrentRoom(function(socketId){
+        if (socketId !== socket.id) {
+            emitPeerEvent(socketId, "receive_chat_msg", {
+                "messages": data.messages,
+                "color": data.color
+              });
+        }
+    });
 });
+
+webRTC.rtc.on('raise_hand', function(data, socket){
+
+    // Iterating thru the rooms has no meaning at this point. It is done only to avoid doing it later, if more than one room support is actually added.
+    doForCurrentRoom(function(socketId){
+        emitPeerEvent(socketId, "hand_raised", {"socketId": socket.id} );
+    });
+});
+
+
