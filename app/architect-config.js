0edf4c33d76c570a7722b8f84541357010d9a270
Naor Biton <naor.biton@gmail.com>
2013-06-30 16:07:19 +0300

    Messages plugin first RC. I hope this works

--- app/architect-config.js
+++ app/architect-config.js
@@ -11,6 +11,11 @@ module.exports = [
         rootDir: __dirname + "/",
         port: process.env.PORT || 3000
     },
-    "./plugins/real-time"
-//    "./plugins/messages"
+    "./plugins/real-time",
+    {
+        packagePath: "./plugins/messages",
+        "socket.io": require('socket.io'),
+        "session.socket.io": require('session.socket.io')
+    }
+
 ];
